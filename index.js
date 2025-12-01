da22cf2fa963f03722ed5caf3ee46bc29181bea1
Shraddha <shraddhabhattarai48@gmail.com>
2025-12-01 08:02:17 +0545

    byehello

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("This is Day 1 of the coding challenge.");  
+console.log("Let's get started with some JavaScript!");
+console.log("Stay tuned for more challenges ahead.");
+console.log("Happy coding!");
