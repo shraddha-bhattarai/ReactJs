4f9afef7cf3dbbbdacfd8076b7d494aa2d809730
Shraddha <shraddhabhattarai48@gmail.com>
2025-12-07 07:45:23 +0545

    feast: j basics

--- day1.js
+++ day1.js
@@ -3,3 +3,4 @@ console.log("This is Day 1 of the coding challenge.");
 console.log("Let's get started with some JavaScript!");
 console.log("Stay tuned for more challenges ahead.");
 console.log("Happy coding!");   
+console.log
